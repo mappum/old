1f370b809fa6dcc2693b485a3c847aad6ac0a7b2
Matt Bell <mappum@gmail.com>
2016-04-30 23:57:21 -0700

    Copy properties and prototype from Class to WrappedClass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 module.exports = function (Class) {
-  return function (...args) {
+  function WrappedClass (...args) {
     return new Class(...args)
   }
+  Object.assign(WrappedClass, Class)
+  WrappedClass.prototype = Class.prototype
+  return WrappedClass
 }
+
+var nonew = require('./'); class C { constructor (a) { this.a = a }; foo (a) { return this.a + a } }
